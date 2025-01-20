b5d7d73fcea83ead3399cf804cefb081e2212cd4
Diego Reis <diegoreso@me.com>
2025-01-20 16:34:00 -0300

    multiplication table exercise

---
+++ projeto_tabuada/Tabuada.java
@@ -0,0 +1,16 @@
+package projeto_tabuada;
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        System.out.println("Entre com o numero para a tabuada");
+        Scanner scanner = new Scanner(System.in);
+
+        int numero = scanner.nextInt();
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + numero * i );
+        }
+
+    }
+}
