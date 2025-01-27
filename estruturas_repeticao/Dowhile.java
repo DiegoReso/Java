45786410b024051d5852a4ea444336cac6b8575e
Diego Reis <diegoreso@me.com>
2025-01-27 16:26:33 -0300

    DoWhile

---
+++ estruturas_repeticao/Dowhile.java
@@ -0,0 +1,14 @@
+package estruturas_repeticao;
+
+public class Dowhile {
+    public static void main(String[] args) {
+        
+        int i  = 10;
+
+        do {
+            System.out.println("Do while imprimindo " + i);
+            i--; 
+        } while (i != 2);
+
+    }
+}
