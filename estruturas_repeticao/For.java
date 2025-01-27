cd2ccac3512cd9afd48107ed5722bf32a3cd7148
Diego Reis <diegoreso@me.com>
2025-01-27 16:10:35 -0300

    studying repetition control structure

---
+++ estruturas_repeticao/For.java
@@ -0,0 +1,19 @@
+package estruturas_repeticao;
+
+public class For {
+    public static void main(String[] args) {
+
+        int[] numeros = {1,2,3,4,5,6};
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+    
+        String palavra = "Java";
+
+        for (int i = 0; i < palavra.length(); i++) {
+            System.out.println("Caractere: " + palavra.charAt(i));
+        }
+        
+    }
+}
