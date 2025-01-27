6de4506b6b3742a5b253fc4a2e6110edef0a9096
Diego Reis <diegoreso@me.com>
2025-01-27 18:10:50 -0300

    exercise 14

---
+++ exercicios/exe14.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+public class exe14 {
+    public static void main(String[] args) {
+        
+        int i = 0;
+
+        while(i <= 20){
+
+            if(i % 2 ==0 && i != 0){
+                System.out.println(i + " Eh Par");
+            }
+        
+            i++;
+        }
+
+    }
+}
