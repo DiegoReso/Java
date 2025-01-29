d0415acc672901045da2b39c31cef71ec46472b0
Diego Reis <diegoreso@me.com>
2025-01-29 12:57:06 -0300

    exercise 22

---
+++ exercicios/exe22.java
@@ -0,0 +1,22 @@
+package exercicios;
+
+import java.util.Arrays;
+
+public class exe22 {
+    public static void main(String[] args) {
+        
+        int [] numeros = {-1,2,3,4,-9};
+
+        for (int i = 0; i < numeros.length; i++) {
+            if(numeros[i] < 0){
+                numeros[i] = 0;
+            }
+        }
+
+        for (int i : numeros) {
+            System.out.println(i);
+        }
+
+
+    }
+}
