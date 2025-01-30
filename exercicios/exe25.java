41a25d6f614cbff327e18131cfd2c49aba39d315
Diego Reis <diegoreso@me.com>
2025-01-30 10:30:27 -0300

    exercise 25 array and loop

--- exercicios/exe25.java
+++ exercicios/exe25.java
@@ -8,28 +8,31 @@ public class exe25 {
         
         int[] numerosCorretos = {5,4,3,2,1};
         
-        inverterArray(numerosCorretos);
-
-        for (int i : numerosCorretos) {
-            System.out.println(i);
-        }
+    
+        
+        System.out.println(Arrays.toString(inverterArray(numerosCorretos)));
     }
     
-    public static void inverterArray(int[] numeros){
+    public static int[] inverterArray(int[] numeros){
 
         int inicio = 0;
         int fim = numeros.length -1;
-
+        int [] numerosCopy = new  int[numeros.length];
 
         while(inicio < fim){
+            
             int temp =  numeros[inicio];
             numeros[inicio] = numeros[fim];
             numeros[fim] = temp;
 
+            for (int i = 0; i < numerosCopy.length; i++) {
+                numerosCopy[i] = numeros[i];
+            }
+            
             inicio++;
             fim--;
         }
 
-        System.out.println();
+        return numerosCopy;
     }
 }
