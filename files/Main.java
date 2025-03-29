0b5204b991ac8583ae2c82bdd32229b1fe8e131e
Diego Reis <diegoreso@me.com>
2025-03-29 12:03:59 -0300

    writing file

--- files/Main.java
+++ files/Main.java
@@ -33,5 +33,28 @@ public class Main {
         } catch (Exception e) {
             System.out.println("Nao foi possivel ler o arquivo!");
         }
+
+        try (FileWriter writer = new FileWriter(currentDir + "saida.txt")) {
+            
+            writer.write("Escrevendo em arquivo \n");
+            writer.write("Escrevendo em arquivo 2");
+
+        } catch (Exception e) {
+            
+            System.out.println("Erro ao escrever em arquivo " + e.getMessage());
+
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(currentDir + "saida2.txt"))) {
+            
+            writer.write("Escrevendo com BufferedWriter");
+            writer.newLine();
+            writer.write("EScrevendo linha 2");
+            writer.newLine();
+            writer.append("Testando append");
+            
+        } catch (Exception e) {
+            System.out.println("Nao foi possivel escrever no arquivo bom BufferedWriter");
+        }
     }
 }
