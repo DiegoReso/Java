0cca180c372a1994a1d4e382a8cf6fad6a5cc765
Diego Reis <diegoreso@me.com>
2025-03-30 21:13:11 -0300

    deserializable studies

--- serializando/Main.java
+++ serializando/Main.java
@@ -1,6 +1,8 @@
 package serializando;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Main {
@@ -14,8 +16,21 @@ public class Main {
         
         try(ObjectOutputStream objetoSerializado = new ObjectOutputStream(new FileOutputStream(currentDir + "pessoa.ser"))) {
             
+            objetoSerializado.writeObject(pessoa);
+            System.out.println("Objeto Serializado com sucesso!");
         } catch (Exception e) {
             System.out.println("Nao foi possivel serializar o arquivo");
         }
+
+        try (ObjectInputStream objetoDeserializado = new ObjectInputStream(new FileInputStream(currentDir + "pessoa.ser"))) {
+            
+            Pessoa pessoaDeserializada = (Pessoa) objetoDeserializado.readObject();
+
+            System.out.println("Nome Deserializado: " + pessoaDeserializada.getNome() + " e idade: " + pessoaDeserializada.getIdade());
+
+        } catch (Exception e) {
+            System.out.println("Nao foi possivel deserializar o arquivo!");
+        }
+        
     }
 }
