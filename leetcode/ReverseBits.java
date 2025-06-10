4feb883de7ec85c08d1714e56dafc63db61165d7
Diego Reis <diegoreso@me.com>
2025-06-10 14:17:18 -0300

    feat: creates leetcode ReverseBits

---
+++ leetcode/ReverseBits.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class ReverseBits {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int reverseBits(int n) {
+        int result = 0;
+
+        for(int i = 0; i < 32 ; i++){
+            result <<=1;
+
+            result += (n & 1);
+            n >>=1;
+        }
+        return result;
+    }
+}
