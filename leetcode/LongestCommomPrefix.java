2241a4eaae585182ea71384306d5af0c0245a4af
Diego Reis <diegoreso@me.com>
2025-06-26 10:18:29 -0300

    feat: creates leetcode LongestCommomPrefix, function to find the longest common prefix string amongst an array of strings.If there is no common prefix, returnan empty string

--- leetcode/LongestCommomPrefix.java
+++ leetcode/LongestCommomPrefix.java
@@ -32,6 +32,3 @@ public class LongestCommomPrefix {
         return commomPrefix.toString();
     }
 }
-
-function to find the longest common prefix string amongst an array of strings.
-If there is no common prefix, return an empty string
